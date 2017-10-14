dc35133f1294fc4cca629831d1abbdad8ea006d5
thestormykid <hanugautam96@gmail.com>
2017-10-14 20:51:55 +0530

    hanu commit

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var express 		= require('express'),
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended :true }));
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
