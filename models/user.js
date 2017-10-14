b2489b3cbb6d4d7a13d3e17474c02f51237b821d
thestormykid <hanugautam96@gmail.com>
2017-10-14 20:20:44 +0530

    Hanu 1st Commit

--- models/user.js
+++ models/user.js
@@ -1,8 +1,29 @@
 var mongoose = require('mongoose');
 
 var userSchema = mongoose.schema({
+	name: String,
+	age: Number,
+	Sex: String,
+	createdAt: { type: Date default: Date().now },
+	prescription: [
+		{
+			prescriptionId: String,
+			doctorId: String,
+			doctor_name: String,
+			hospital_name: String,
+			prescriptionDetails: String,
+			createdAt: { type: Date default: Date().now }
+			diseases : []
+		}
+	],
+	reports: [
+		{
+			reportId: String,
+			reportName: String,
+			uploadedAt: { type: Date default: Date().now }
+		}
+	]
 
-  name: String
 });
 
-module.exports = mongoose.model('User', productSchema);
+module.exports = mongoose.model('User', userSchema);
